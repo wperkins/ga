531e3a8df89b63ca8bcb3f73c25f881353e90a8c
Joseph Manzano <joseph.manzano@pnnl.gov>
2018-07-31 16:51:57 -0700

    Bug about the thread id wrap around -J

--- global/src/thread-safe.c
+++ global/src/thread-safe.c
@@ -18,10 +18,10 @@ int32_t *tids; /* map of system ids to numerical ids */
 
 int64_t get_tid(){
    if(tids == NULL) return -1;
-   int i;
+   int i, cnt;
    int64_t id = GET_SWID() % max_threads;
-   for(i = id; tids[i] != GET_SWID() && i <  max_threads; i++); 
-   if(i < max_threads) return i;
+   for(i = id, cnt = 0; tids[i] != GET_SWID() && cnt <  max_threads; cnt++, i=(i+1)%max_threads); 
+   if(cnt < max_threads) return i;
    return -1;
 }
 
@@ -30,7 +30,7 @@ int64_t get_tid(){
 #include <sys/resource.h>
 
 int set_thread_config(){
-   int i;
+   int i, cnt;
    struct rlimit rlim;
    if(tids != NULL) return -2;
    //int64_t mthread = get_nprocs();
@@ -42,8 +42,8 @@ int set_thread_config(){
    GA_Internal_Threadsafe_Lock(FAT_LOCK);
    for(i = 0; i < mthread; ++i) tids[i] = -1;
    int64_t ty = GET_SWID() % mthread;
-   for(i = ty; tids[i] != -1 && i <  mthread; i++); 
-   if(i < mthread) {
+   for(i = ty, cnt = 0; tids[i] != -1 && cnt <  mthread; cnt++, i = (i+1) % mthread); 
+   if(cnt < mthread) {
        tids[i] = GET_SWID();
        GA_Internal_Threadsafe_Unlock(FAT_LOCK);
        return 0;

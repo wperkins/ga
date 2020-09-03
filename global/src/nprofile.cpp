ed48fe1691095539e632e9d1b938b9e73cbdfb61
JonnySQ <sqjc329@gmail.com>
2020-09-03 02:42:47 -0700

    string function hash implemented

--- global/src/nprofile.cpp
+++ global/src/nprofile.cpp
@@ -4,25 +4,23 @@
 #include <functional> //for hash
 #include <chrono>     //for time
 #include <ctime>      //for time
-#include <iostream>
+#include <string.h>   //for string
 #include <stdlib.h>
 #include <unistd.h>
 
 using namespace std;
 
-//(hash???)
 struct GA_perf_thread {
-   int tid;                    //thread id
-   stack<int> entries;         //tracks order of function calls and nesting
-   vector<int> function_times; //"hash" (keys are the indicies, values are the times)
-   //hash that includes ints for function key with a corresponding time performance measurement          
+   int tid;                       //thread id
+   stack<int> entries;            //tracks order of function calls and nesting
+   vector<int> function_times;    //(keys are indicies, values are the times)
+   vector<string> function_names; //(function names link indicies to times)
 };
 
 struct GA_perf_node {
   int nid;                     //node id
   int nthreads;                //number of threads
   vector<GA_perf_thread> perf_threads; //reference to all thread structures
-
 };
 
 struct GA_perf_main {
@@ -48,23 +46,38 @@ GA_perf_thread create_thread(int tid, GA_perf_node entry)
   	return perf_thread;
 };
 
+
+int get_func_hash_key(int nid, int tid, string hash_key_name)
+{
+  for (int i = 0; i < perf_main.perf_nodes[nid].perf_threads[tid].function_names.size(); i++)
+    if (perf_main.perf_nodes[nid].perf_threads[tid].function_names[i] == hash_key_name)
+      return i;
+  return -1;
+};
+
+
 //seems to be COMPLETE
 //adds an entry of when a function starting or ending
             //node id, thread id, tells what fuction it is, time metric, entry or exit
-int add_entry(int nid, int tid, int func_hash_key, /*enum METRIC*/ int m, bool exit)
+int add_entry(int nid, int tid, string func_hash_name, /*enum METRIC*/ int m, bool exit)
 {
-	
 	//do node and thread exist?
 	if (nid > perf_main.perf_nodes.size())
 	  return -1;
 	
 	if (tid > perf_main.perf_nodes[nid].perf_threads.size())
       return -1;
+    
+	int func_hash_key = get_func_hash_key(nid, tid, func_hash_name); //placeholder
 	
-	//return time if entry has already been tested...? does time need to be returned?
-	while (perf_main.perf_nodes[nid].perf_threads[tid].function_times.size() <= func_hash_key)
+    //creates space for new function time to measure
+	if (perf_main.perf_nodes[nid].perf_threads[tid].function_times.size() <= func_hash_key)
+	{
 		perf_main.perf_nodes[nid].perf_threads[tid].function_times.push_back(0);
+		perf_main.perf_nodes[nid].perf_threads[tid].function_names.push_back(func_hash_name);
+	}
 	
+	//return time if entry has already been tested...? does time need to be returned?	
 	if (perf_main.perf_nodes[nid].perf_threads[tid].function_times[func_hash_key] != 0)
 	  return perf_main.perf_nodes[nid].perf_threads[tid].function_times[func_hash_key];
 	
@@ -73,8 +86,6 @@ int add_entry(int nid, int tid, int func_hash_key, /*enum METRIC*/ int m, bool e
       unsigned long track_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch()).count();
       //unsigned long track_time = std::clock();
       perf_main.perf_nodes[nid].perf_threads[tid].entries.push(track_time);
-   
-
     }
 
     else if (exit == true)
@@ -93,30 +104,40 @@ int add_entry(int nid, int tid, int func_hash_key, /*enum METRIC*/ int m, bool e
 };
 
 //calculate function time for single thread
-unsigned long accumulate_thread_value(int nid, int tid, /*enum METRIC*/ int m, int func_hash_key)
+unsigned long accumulate_thread_value(int nid, int tid, /*enum METRIC*/ int m, string func_hash_name)
 {
-	return perf_main.perf_nodes[nid].perf_threads[tid].function_times[func_hash_key];
+    int func_hash_key = get_func_hash_key(nid, tid, func_hash_name); 
+    if (func_hash_key > -1)
+	  return perf_main.perf_nodes[nid].perf_threads[tid].function_times[func_hash_key];
 };
 
 //calculate function time for node (all threads)
-unsigned long accumulate_node_value(int nid, /*enum METRIC*/ int m, int func_hash_key)
+unsigned long accumulate_node_value(int nid, /*enum METRIC*/ int m, string func_hash_name)
 {
+    
   unsigned long total = 0;
   
   for (int i = 0; i < perf_main.perf_nodes[nid].perf_threads.size(); i++)
+  {
+    int func_hash_key = get_func_hash_key(nid, i, func_hash_name);
+    if (func_hash_key > -1)
 	  total += perf_main.perf_nodes[nid].perf_threads[i].function_times[func_hash_key];
-  
+  }
   return total;
 };
 
 //calculate function time for all nodes (and all threads)
-unsigned long accumulate_total_value(/*enum METRIC*/ int m, int func_hash_key)
+unsigned long accumulate_total_value(/*enum METRIC*/ int m, string func_hash_name)
 {
   unsigned long total = 0;
-  
+
   for (int i = 0; i < perf_main.perf_nodes.size(); i++)
-      for (int j = 0; j < perf_main.perf_nodes[i].perf_threads.size(); j++)
-	      total += perf_main.perf_nodes[i].perf_threads[j].function_times[func_hash_key];
+    for (int j = 0; j < perf_main.perf_nodes[i].perf_threads.size(); j++)
+    {
+        int func_hash_key = get_func_hash_key(i, j, func_hash_name);
+        if (func_hash_key > -1)
+	    total += perf_main.perf_nodes[i].perf_threads[j].function_times[func_hash_key];
+    }
   
   return total;
 };
@@ -152,7 +173,7 @@ void functionB() {
    usleep(10000);
 };
 
-//basic test for
+//basic test
 int main(){
 
   int tid = 0; 
@@ -172,44 +193,21 @@ int main(){
 	}
   }
   
-  add_entry(nid, tid, 0, 0, 0);
+  add_entry(nid, tid, "functionA", 0, 0);
   functionA();
-  add_entry(nid, tid, 0, 0, 1);
+  add_entry(nid, tid, "functionA", 0, 1);
   
-  add_entry(nid, tid, 1, 0, 0);
+  add_entry(nid, tid, "functionB", 0, 0);
   functionB();
-  add_entry(nid, tid, 1, 0, 1);
+  add_entry(nid, tid, "functionB", 0, 1);
   
-  unsigned long thread_time = accumulate_thread_value(nid,tid,0,0);
-  unsigned long node_time = accumulate_node_value(nid,0,0);
-  unsigned long total_time = accumulate_total_value(0,0);
+  unsigned long thread_time = accumulate_thread_value(nid,tid,0,"functionA");
+  unsigned long node_time = accumulate_node_value(nid,0,"functionA");
+  unsigned long total_time = accumulate_total_value(0,"functionA");
   
-  /***
-  printf("\n value test function 0");
-  printf("\n thread time: %lu", thread_time);
-  printf("\n node time:   %lu", node_time);
-  printf("\n total time:  %lu", total_time);
-  printf("\n value test function 0 complete \n");
-  ***/
-  
-  thread_time = accumulate_thread_value(nid,tid,0,1);
-  node_time = accumulate_node_value(nid,0,1);
-  total_time = accumulate_total_value(0,1);
-  
-  /***
-  printf("\n value test function 1");
-  printf("\n thread time: %lu", thread_time);
-  printf("\n node time:   %lu", node_time);
-  printf("\n total time:  %lu", total_time);
-  printf("\n value test function 1 complete \n");
-  ***/
-  
-  /***
-  printf("\n test vector stats");
-  printf("\n node vector size:   %lu", perf_main.perf_nodes.size());
-  printf("\n thread vector size: %lu", perf_main.perf_nodes[0].perf_threads.size());
-  printf("\n test vector stats complete \n");
-  ***/
+  thread_time = accumulate_thread_value(nid,tid,0,"functionB");
+  node_time = accumulate_node_value(nid,0,"functionB");
+  total_time = accumulate_total_value(0,"funcionB");
   
   print_full_values();
 
